264447ed116d1e5baaf5fcd22f263d63b8d27635
Kenie B <kbett001@gmail.com>
2021-12-20 03:28:29 -0500

    /dishes/:dishId GET(read) complete and passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -24,6 +24,10 @@ function create(req, res) {
   res.status(201).json({ data: newDish });
 }
 
+function read(req,res) {
+    res.json({ data: res.locals.dish });
+}
+
 //Request Validations
 function bodyHasNameProperty(req, res, next) {
   const { data: { name } = {} } = req.body;
@@ -96,6 +100,20 @@ function bodyHasImageUrlProperty(req, res, next) {
   });
 }
 
+//Validations for existing dish
+function dishExists(req,res,next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === dishId);
+    if(foundDish) {
+        res.locals.dish = foundDish;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Dish id not found: ${dishId}`,
+    });
+}
+
 module.exports = {
   list,
   create: [
@@ -106,4 +124,5 @@ module.exports = {
     bodyHasImageUrlProperty,
     create,
   ],
+  read: [dishExists, read],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -4,6 +4,7 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
+router.route("/:dishId").get(controller.read).all(methodNotAllowed);
 
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
