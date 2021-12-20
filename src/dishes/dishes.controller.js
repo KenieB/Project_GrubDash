d81beef785e8339171025d782fcff53afe71cffc
Kenie B <kbett001@gmail.com>
2021-12-20 03:12:23 -0500

    C:/Program Files/Git/dishes GET(list) and POST(create) complete and passing

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,103 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function create(req, res) {
+  const newDishId = nextId();
+  const newDish = {
+    id: newDishId,
+    name: res.locals.reqName,
+    description: res.locals.reqDescription,
+    price: res.locals.reqPrice,
+    image_url: res.locals.reqImageUrl,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+//Request Validations
+function bodyHasNameProperty(req, res, next) {
+  const { data: { name } = {} } = req.body;
+  if (name) {
+    res.locals.reqName = name;
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a name",
+  });
+}
+
+function bodyHasDescriptionProperty(req, res, next) {
+  const { data: { description } = {} } = req.body;
+  if (description) {
+    res.locals.reqDescription = description;
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a description",
+  });
+}
+
+function bodyHasPriceProperty(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price) {
+    return next();
+  }
+  if (price === 0) {
+    next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  }
+  next({
+    status: 400,
+    message: "Dish must include a price",
+  });
+}
+
+function pricePropertyIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (typeof price !== "number") {
+    next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  } else if (price < 0 || price % 1 !== 0) {
+    next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  } else {
+    res.locals.reqPrice = price;
+    next();
+  }
+}
+
+function bodyHasImageUrlProperty(req, res, next) {
+  const { data: { image_url } = {} } = req.body;
+  if (image_url) {
+    res.locals.reqImageUrl = image_url;
+    return next();
+  }
+  next({
+    status: 400,
+    message: "Dish must include a image_url",
+  });
+}
+
+module.exports = {
+  list,
+  create: [
+    bodyHasNameProperty,
+    bodyHasDescriptionProperty,
+    bodyHasPriceProperty,
+    pricePropertyIsValid,
+    bodyHasImageUrlProperty,
+    create,
+  ],
+};
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
